391ca35d793f2ef7b6802b3b32b848341af9e7eb
stako <stako@users.noreply.github.com>
2024-03-12 13:55:04 -0700

    update #winner rspec example group

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -4,10 +4,9 @@ require_relative "../lib/connect_four"
 
 describe ConnectFour do
   subject(:game) { described_class.new }
+  let(:grid) { game.instance_variable_get(:@grid) }
 
   describe "#full_grid?" do
-    let(:grid) { game.instance_variable_get(:@grid) }
-
     it "returns true if grid is completely filled" do
       grid.each { |column| column.map! { true } }
       expect(game.full_grid?).to be true
@@ -19,15 +18,76 @@ describe ConnectFour do
   end
 
   describe "#winner" do
-    xit "returns #{described_class::WHITE_TOKEN} if #{described_class::WHITE_TOKEN} has won" do
-      expect(game.winner).to eq(described_class::WHITE_TOKEN)
+    white = described_class::WHITE_TOKEN
+    black = described_class::BLACK_TOKEN
+
+    it "returns #{white} when #{white} has 4 in a row vertically" do
+      4.times { |i| grid[0][i] = white }
+      expect(game.winner).to eq(white)
     end
 
-    xit "returns #{described_class::BLACK_TOKEN} if #{described_class::BLACK_TOKEN} has won" do
-      expect(game.winner).to eq(described_class::BLACK_TOKEN)
+    it "returns nil when #{white} has 4 NOT in a row vertically" do
+      2.times { |i| grid[0][i] = white }
+      (3..4).each { |i| grid[0][i] = white }
+      expect(game.winner).to be_nil
     end
 
-    xit "returns nil if there is no winner" do
+    it "returns #{white} when #{white} has 4 in a row horizontally" do
+      4.times { |i| grid[i][0] = white }
+      expect(game.winner).to eq(white)
+    end
+
+    it "returns nil when #{white} has 4 NOT in a row horizontally" do
+      2.times { |i| grid[i][0] = white }
+      (3..4).each { |i| grid[i][0] = white }
+      expect(game.winner).to be_nil
+    end
+
+    it "returns #{white} when #{white} has 4 in a row diagonally" do
+      4.times { |i| grid[i][i] = white }
+      expect(game.winner).to eq(white)
+    end
+
+    it "returns nil when #{white} has 4 NOT in a row diagonally" do
+      2.times { |i| grid[i][i] = white }
+      (3..4).each { |i| grid[i][i] = white }
+      expect(game.winner).to be_nil
+    end
+
+    it "returns #{black} when #{black} has 4 in a row vertically" do
+      4.times { |i| grid[0][i] = black }
+      expect(game.winner).to eq(black)
+    end
+
+    it "returns nil when #{black} has 4 NOT in a row vertically" do
+      2.times { |i| grid[0][i] = black }
+      (3..4).each { |i| grid[0][i] = black }
+      expect(game.winner).to be_nil
+    end
+
+    it "returns #{black} when #{black} has 4 in a row horizontally" do
+      4.times { |i| grid[i][0] = black }
+      expect(game.winner).to eq(black)
+    end
+
+    it "returns nil when #{black} has 4 NOT in a row horizontally" do
+      2.times { |i| grid[i][0] = black }
+      (3..4).each { |i| grid[i][0] = black }
+      expect(game.winner).to be_nil
+    end
+
+    it "returns #{black} when #{black} has 4 in a row diagonally" do
+      4.times { |i| grid[i][i] = black }
+      expect(game.winner).to eq(black)
+    end
+
+    it "returns nil when #{black} has 4 NOT in a row diagonally" do
+      2.times { |i| grid[i][i] = black }
+      (3..4).each { |i| grid[i][i] = black }
+      expect(game.winner).to be_nil
+    end
+
+    it "returns nil when there are no winning combos" do
       expect(game.winner).to be_nil
     end
   end
