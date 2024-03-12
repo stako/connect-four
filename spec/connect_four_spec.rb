2fc0a4ee0b2890c9a0f87b267b10796feb415012
stako <stako@users.noreply.github.com>
2024-03-12 13:10:34 -0700

    add #winner example group

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -17,4 +17,18 @@ describe ConnectFour do
       expect(game.full_grid?).to be false
     end
   end
+
+  describe "#winner" do
+    xit "returns #{described_class::WHITE_TOKEN} if #{described_class::WHITE_TOKEN} has won" do
+      expect(game.winner).to eq(described_class::WHITE_TOKEN)
+    end
+
+    xit "returns #{described_class::BLACK_TOKEN} if #{described_class::BLACK_TOKEN} has won" do
+      expect(game.winner).to eq(described_class::BLACK_TOKEN)
+    end
+
+    xit "returns nil if there is no winner" do
+      expect(game.winner).to be_nil
+    end
+  end
 end
