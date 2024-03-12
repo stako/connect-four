210f6a11d968424131231e67234db4e217106820
stako <stako@users.noreply.github.com>
2024-03-12 15:24:32 -0700

    add #winner methods

--- lib/connect_four/game.rb
+++ lib/connect_four/game.rb
@@ -12,5 +12,59 @@ module ConnectFour
       @grid.each { |column| return true unless column.any?(nil) }
       false
     end
+
+    def winner
+      vertical_winner || horizontal_winner || diagonal_winner
+    end
+
+    private
+
+    def vertical_winner
+      @grid.each do |column|
+        white_combo = 0
+        black_combo = 0
+        column.each do |row|
+          return WHITE_TOKEN if white_combo == 4
+          return BLACK_TOKEN if black_combo == 4
+
+          row == WHITE_TOKEN ? white_combo += 1 : white_combo = 0
+          row == BLACK_TOKEN ? black_combo += 1 : black_combo = 0
+        end
+      end
+      nil
+    end
+
+    def horizontal_winner
+      6.times do |row|
+        white_combo = 0
+        black_combo = 0
+        7.times do |column|
+          return WHITE_TOKEN if white_combo == 4
+          return BLACK_TOKEN if black_combo == 4
+
+          @grid[column][row] == WHITE_TOKEN ? white_combo += 1 : white_combo = 0
+          @grid[column][row] == BLACK_TOKEN ? black_combo += 1 : black_combo = 0
+        end
+      end
+      nil
+    end
+
+    def diagonal_winner
+      @grid.each_index do |column|
+        @grid[column].each_index do |row|
+          white_combo = 0
+          black_combo = 0
+          4.times do |i|
+            break if @grid[column + i].nil? || @grid[column + i][row + i].nil?
+
+            white_combo += 1 if @grid[column + i][row + i] == WHITE_TOKEN
+            black_combo += 1 if @grid[column + i][row + i] == BLACK_TOKEN
+          end
+          return WHITE_TOKEN if white_combo == 4
+          return BLACK_TOKEN if black_combo == 4
+        end
+      end
+      nil
+    end
   end
 end
