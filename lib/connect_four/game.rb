d3e8beb137d7ed475514d4db87dc576d52fe690c
stako <stako@users.noreply.github.com>
2024-03-12 16:00:59 -0700

    refactor #winner methods

--- lib/connect_four/game.rb
+++ lib/connect_four/game.rb
@@ -14,55 +14,31 @@ module ConnectFour
     end
 
     def winner
-      vertical_winner || horizontal_winner || diagonal_winner
+      @grid.each_with_index do |column, x|
+        column.each_index do |y|
+          token = @grid[x][y]
+          next if token.nil?
+
+          return token if find_combo(x, y, token)
+        end
+      end
+      nil
     end
 
     private
 
-    def vertical_winner
-      @grid.each do |column|
-        white_combo = 0
-        black_combo = 0
-        column.each do |row|
-          return WHITE_TOKEN if white_combo == 4
-          return BLACK_TOKEN if black_combo == 4
-
-          row == WHITE_TOKEN ? white_combo += 1 : white_combo = 0
-          row == BLACK_TOKEN ? black_combo += 1 : black_combo = 0
+    def find_combo(x, y, token)
+      directions = [[1, 0], [0, 1], [1, 1], [1, -1]]
+      directions.each do |dx, dy|
+        combo = 1
+        new_x = x + dx
+        new_y = y + dy
+        while combo < 4 && @grid[new_x] && @grid[new_x][new_y] == token
+          combo += 1
+          new_x += dx
+          new_y += dy
         end
-      end
-      nil
-    end
-
-    def horizontal_winner
-      6.times do |row|
-        white_combo = 0
-        black_combo = 0
-        7.times do |column|
-          return WHITE_TOKEN if white_combo == 4
-          return BLACK_TOKEN if black_combo == 4
-
-          @grid[column][row] == WHITE_TOKEN ? white_combo += 1 : white_combo = 0
-          @grid[column][row] == BLACK_TOKEN ? black_combo += 1 : black_combo = 0
-        end
-      end
-      nil
-    end
-
-    def diagonal_winner
-      @grid.each_index do |column|
-        @grid[column].each_index do |row|
-          white_combo = 0
-          black_combo = 0
-          4.times do |i|
-            break if @grid[column + i].nil? || @grid[column + i][row + i].nil?
-
-            white_combo += 1 if @grid[column + i][row + i] == WHITE_TOKEN
-            black_combo += 1 if @grid[column + i][row + i] == BLACK_TOKEN
-          end
-          return WHITE_TOKEN if white_combo == 4
-          return BLACK_TOKEN if black_combo == 4
-        end
+        return token if combo == 4
       end
       nil
     end
